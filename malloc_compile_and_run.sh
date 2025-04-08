ba4115fca65d8112b311d9edc1f567f9fc9d9a83
nefir2 <vadim.ahtamov@gmail.com>
2025-04-08 09:05:26 +0300

    compile script fixes

--- malloc_compile_and_run.sh
+++ malloc_compile_and_run.sh
@@ -1,2 +1,18 @@
-gcc src/wndtn_malloc.c -o bin/wndtn_malloc; 
-./bin/wndtn_malloc
+#!/bin/bash
+
+export filename='wndtn_malloc';
+export fileext='.c';
+export srcdir='src';
+export bindir='bin';
+export compilername='gcc';
+
+command -v $compilername;
+if [[ $? -ne 0 ]]; then
+	echo "${compilername} not found in your system.";
+	exit 1;
+fi
+
+gcc "${srcdir}/${filename}${fileext}" -o "${bindir}/${filename}_${compilername}"; 
+command `pwd`/$bindir/"${filename}_${compilername}";
+
+exit 0;
