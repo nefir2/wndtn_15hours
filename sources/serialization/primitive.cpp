bba24ce37e4d965f9e896b22ef11df472e86e83f
nefir2 <vadim.ahtamov@gmail.com>
2025-04-10 17:39:32 +0300

    оказывается он сделал это за глазами и не слова не сказал насчёт этого.

--- sources/serialization/primitive.cpp
+++ sources/serialization/primitive.cpp
@@ -7,9 +7,9 @@ namespace ObjectModel {
 	}
 
 	void Primitive::pack(std::vector<int8_t>* buffer, int16_t* iterator) {
-		Core::encode<std::string>(buffer, iterator, name);
-		Core::encode<int16_t>(buffer, iterator, nameLength);
 		Core::encode<int8_t>(buffer, iterator, wrapper);
+		Core::encode<int16_t>(buffer, iterator, nameLength);
+		Core::encode<std::string>(buffer, iterator, name);
 		Core::encode<int8_t>(buffer, iterator, type);
 		Core::encode<int8_t>(buffer, iterator, *data);
 		Core::encode<int32_t>(buffer, iterator, size);
