78004f220d3fd0a09cf92a8a44b350c26bedede8
nefir2 <vadim.ahtamov@gmail.com>
2025-04-10 20:28:24 +0300

    doesn't buildig, but pofek

--- compile_scripts/client_server_compile_and_run.sh
+++ compile_scripts/client_server_compile_and_run.sh
@@ -15,6 +15,7 @@ fi;
 command explorer server.exe;
 command explorer client.exe;
 java ../sources/client/java/Client.java;
+dotnet run ../sources/client/csharp/Servers_Client.csproj;
 
 # echo -e "building java client . . . \n";
 # javac ../sources/client/java/Client.java && jar cvf client.jar Client.class && 
